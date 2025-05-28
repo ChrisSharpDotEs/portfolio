896595e3644a765dc93a7aec4e31bbb4a64a56c1
Cristian <82599146+ChrisSharpDotEs@users.noreply.github.com>
2025-05-28 11:18:46 +0100

    Update

--- resources/js/Modal.js
+++ resources/js/Modal.js
@@ -1,86 +1,91 @@
 import { LitElement, html } from 'lit';
 
 export class LitModal extends LitElement {
-    static properties = {
-        abierto: { type: Boolean },
-        cerrando: { type: Boolean },
-    };
+  static properties = {
+    opening: { type: Boolean },
+    closing: { type: Boolean },
+  };
 
-    constructor() {
-        super();
-        this.abierto = false;
-        this.cerrando = false;
-    }
+  constructor() {
+    super();
+    this.opening = true;
+    this.closing = false;
+  }
 
-    createRenderRoot() {
-        return this;
-    }
+  createRenderRoot() {
+    return this;
+  }
 
-    abrir() {
-        this.abierto = true;
-        this.cerrando = false;
-    }
+  open() {
+    this.opening = true;
+    this.closing = false;
+  }
 
-    cerrar() {
-        this.cerrando = true;
-        setTimeout(() => {
-            this.abierto = false;
-            this.cerrando = false;
-        }, 300);
-    }
+  close() {
+    this.closing = true;
+    setTimeout(() => {
+      this.opening = false;
+      this.closing = false;
+    }, 300);
+  }
 
-    render() {
-        return html`
-      <button class="bg-blue-600 text-white px-4 py-2 rounded" @click=${this.abrir}>
-        Abrir Modal
-      </button>
+  render() {
+    return html`
+      ${this.opening ? html`${this.#createDomElement()}` : ''}`;
+  }
 
-      ${this.abierto ? html`
-            <div class="fixed inset-0 bg-[#69728463] backdrop-blur-sm flex items-center justify-center z-50">
-              <div class="bg-white p-6 rounded-lg shadow-2xl max-w-sm w-full text-center
-                ${this.cerrando ? 'animate-fade-scale-out' : 'animate-popup-bounce'}">
-                <h2 class="text-2xl font-semibold mb-2 text-green-600">
-                  ¡Éxito!
-                </h2>
-                <p class="mb-4 text-gray-700">
-                  Operación completada correctamente.
-                </p>
+  #createDomElement() {
+    if (!this.opening) return;
 
-                <svg
-                    class="mx-auto mb-4 w-20 h-20"
-                    viewBox="0 0 48 48"
-                    xmlns="http://www.w3.org/2000/svg"
-                    >
-                    <circle cx="24" cy="24" r="22" fill="#D1FAE5" /> <!-- green-100 -->
+    const overlay = document.createElement('div');
+    overlay.className = 'fixed inset-0 bg-[#69728463] backdrop-blur-sm flex items-center justify-center z-50';
 
-                    <!-- Check animado -->
-                    <path
-                        d="M16 24l6 6 12-12"
-                        fill="none"
-                        stroke="#10B981"
-                        stroke-width="4"
-                        stroke-linecap="round"
-                        stroke-linejoin="round"
-                        stroke-dasharray="36"
-                        stroke-dashoffset="36"
-                        style="animation: drawCheck 0.4s ease forwards"
-                    />
-                </svg>
+    const container = document.createElement('div');
+    container.className = `bg-white p-6 rounded-lg shadow-2xl max-w-xl w-full dark:bg-gray-800 dark:text-white
+      ${this.closing ? 'animate-fade-scale-out' : 'animate-popup-bounce'}`;
 
-                <style>
-                    @keyframes drawCheck {
-                        to {
-                            stroke-dashoffset: 0;
-                        }
-                    }
-                </style>
+    const title = document.createElement('h2');
+    title.className = 'text-2xl font-semibold mb-2 text-gray-800 dark:text-gray-200';
+    title.textContent = 'Política de cookies';
 
-                <button
-                  class="mt-2 px-4 py-2 bg-green-500 text-white rounded hover:bg-green-600"
-                  @click=${this.cerrar}>
-                  Cerrar
-                </button>
-              </div>
-            </div>` : ''}`;
-    }
+    const message = document.createElement('p');
+    message.className = 'mb-4 text-gray-700 dark:text-gray-200';
+    message.textContent = 'Esta web solo utiliza cookies necesarias para el correcto funcionamiento del sitio. No utilizamos cookies de publicidad ni de uso del sitio.';
+
+    const button = document.createElement('button');
+    button.className = 'mt-2 px-4 py-2 bg-gray-800 hover:bg-gray-400 text-white rounded dark:bg-gray-200 dark:hover:bg-gray-400 dark:text-gray-800';
+    button.textContent = 'Cerrar';
+    button.addEventListener('click', this.close.bind(this));
+
+    const tooltip = document.createElement('div');
+    tooltip.textContent = 'Cierra aqui';
+    tooltip.className = 'absolute border-2 border-gray-200 px-2 py-1 bg-orange-800 text-white text-sm rounded opacity-0 transition-opacity duration-300 pointer-events-none';
+
+    button.appendChild(tooltip);
+
+    let tooltipTimeout;
+
+    overlay.addEventListener('click', (e) => {
+      if (e.target === overlay) {
+        container.classList.remove('animate-popup-bounce');
+        void container.offsetWidth;
+        container.classList.add('animate-popup-bounce');
+      }
+      tooltip.classList.remove('opacity-0');
+      tooltip.classList.add('opacity-80');
+
+      clearTimeout(tooltipTimeout);
+      tooltipTimeout = setTimeout(() => {
+        tooltip.classList.remove('opacity-80');
+        tooltip.classList.add('opacity-0');
+      }, 2500);
+    });
+
+
+    container.append(title, message, button);
+    overlay.appendChild(container);
+
+    return overlay;
+  }
+
 }
