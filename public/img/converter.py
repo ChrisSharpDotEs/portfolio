728ecbd1f8563f7996b29f0d365c9b4f648f52e8
Cristian <82599146+ChrisSharpDotEs@users.noreply.github.com>
2025-05-28 11:53:43 +0100

    Update

--- public/img/converter.py
+++ public/img/converter.py
@@ -3,7 +3,7 @@ import os
 
 def convertir_jpg_a_webp(directorio):
     for archivo in os.listdir(directorio):
-        if archivo.lower().endswith(".jpg") or archivo.lower().endswith(".jpeg"):
+        if archivo.lower().endswith(".jpg") or archivo.lower().endswith(".png"):
             ruta_jpg = os.path.join(directorio, archivo)
             ruta_webp = os.path.splitext(ruta_jpg)[0] + ".webp"
             with Image.open(ruta_jpg) as img:
