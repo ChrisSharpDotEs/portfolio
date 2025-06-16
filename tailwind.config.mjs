314ec716724a9e4d9480eb8b76be3c19dc11a74b
Cristian <82599146+ChrisSharpDotEs@users.noreply.github.com>
2025-06-16 13:16:22 +0100

    Reestructuracion

---
+++ tailwind.config.mjs
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./templates/pages/*.html",
+    "./templates/auth/*.html",
+    "./templates/*.html",
+    "./static/js/plugins/*.js",
+    "./static/js/*.js"
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
