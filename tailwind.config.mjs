32c1865b54d231f76104d1b298f4dfdd71b29c49
Cristian <82599146+ChrisSharpDotEs@users.noreply.github.com>
2025-08-29 16:06:44 +0100

    Update

---
+++ tailwind.config.mjs
@@ -0,0 +1,12 @@
+// tailwind.config.js
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts}", // ¡Asegúrate de que esta línea esté presente!
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
